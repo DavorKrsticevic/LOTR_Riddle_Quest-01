b88abb08f2f9fe9dec39c5929abdeb90699f1d40
Davor Krsticevic <davor.krsticevic@gmail.com>
2018-10-17 15:23:34 +0000

    Starting with JavaScript/jQuery

---
+++ static/JS/main.js
@@ -0,0 +1,9 @@
+/*global $*/
+
+$(document).ready(function(){
+    $("#imgtab").click(function(){
+        $(this).animate({
+            left: '450px',
+            top: '30px',} , 1500);
+    });
+});
